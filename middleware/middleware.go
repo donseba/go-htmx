582fe70d18f9db8b2fe22efc7361c9c98336e15c
Sebastiano Bellinzis <sebastiano.bellinzis@outlook.com>
2024-01-09 21:16:39 +0100

    instead of removing the middleware add deprecated notice so we can remove it in version 2

---
+++ middleware/middleware.go
@@ -0,0 +1,23 @@
+package middleware
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/donseba/go-htmx"
+)
+
+// MiddleWare is a middleware that adds the htmx request header to the context
+// deprecated: htmx will retrieve the headers from the request by itself using htmx.NewHandler(w, r)
+func MiddleWare(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+
+		hxh := htmx.HxRequestHeaderFromRequest(r)
+
+		ctx = context.WithValue(ctx, htmx.ContextRequestHeader, hxh)
+
+		next.ServeHTTP(w, r.WithContext(ctx))
+	}
+	return http.HandlerFunc(fn)
+}
