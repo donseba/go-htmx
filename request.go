1f77c55ea2b95817e34c1b06429960071f58c5e8
Sebastiano Bellinzis <sebastiano.bellinzis@outlook.com>
2024-01-23 22:01:03 +0100

    some more utility classes to - quickly check if a request is an htmx request . - write strings, html and json - additional get rid of magic strings

--- request.go
+++ request.go
@@ -4,9 +4,23 @@ import (
 	"net/http"
 )
 
-const ContextRequestHeader = "htmx-request-header"
+const (
+	// ContextRequestHeader is the context key for the htmx request header.
+	ContextRequestHeader = "htmx-request-header"
+
+	HxRequestHeaderBoosted               HxRequestHeaderKey = "HX-Boosted"
+	HxRequestHeaderCurrentURL            HxRequestHeaderKey = "HX-Current-URL"
+	HxRequestHeaderHistoryRestoreRequest HxRequestHeaderKey = "HX-History-Restore-Request"
+	HxRequestHeaderPrompt                HxRequestHeaderKey = "HX-Prompt"
+	HxRequestHeaderRequest               HxRequestHeaderKey = "HX-Request"
+	HxRequestHeaderTarget                HxRequestHeaderKey = "HX-Target"
+	HxRequestHeaderTriggerName           HxRequestHeaderKey = "HX-Trigger-Name"
+	HxRequestHeaderTrigger               HxRequestHeaderKey = "HX-Trigger"
+)
 
 type (
+	HxRequestHeaderKey string
+
 	HxRequestHeader struct {
 		HxBoosted               bool
 		HxCurrentURL            string
@@ -21,14 +35,14 @@ type (
 
 func HxRequestHeaderFromRequest(r *http.Request) HxRequestHeader {
 	return HxRequestHeader{
-		HxBoosted:               HxStrToBool(r.Header.Get("HX-Boosted")),
-		HxCurrentURL:            r.Header.Get("HX-Current-URL"),
-		HxHistoryRestoreRequest: HxStrToBool(r.Header.Get("HX-History-Restore-Request")),
-		HxPrompt:                r.Header.Get("HX-Prompt"),
-		HxRequest:               HxStrToBool(r.Header.Get("HX-Request")),
-		HxTarget:                r.Header.Get("HX-Target"),
-		HxTriggerName:           r.Header.Get("HX-Trigger-Name"),
-		HxTrigger:               r.Header.Get("HX-Trigger"),
+		HxBoosted:               HxStrToBool(r.Header.Get(HxRequestHeaderBoosted.String())),
+		HxCurrentURL:            r.Header.Get(HxRequestHeaderCurrentURL.String()),
+		HxHistoryRestoreRequest: HxStrToBool(r.Header.Get(HxRequestHeaderHistoryRestoreRequest.String())),
+		HxPrompt:                r.Header.Get(HxRequestHeaderPrompt.String()),
+		HxRequest:               HxStrToBool(r.Header.Get(HxRequestHeaderRequest.String())),
+		HxTarget:                r.Header.Get(HxRequestHeaderTarget.String()),
+		HxTriggerName:           r.Header.Get(HxRequestHeaderTriggerName.String()),
+		HxTrigger:               r.Header.Get(HxRequestHeaderTrigger.String()),
 	}
 }
 
@@ -41,4 +55,8 @@ func (h *HTMX) HxHeader(r *http.Request) HxRequestHeader {
 
 	// if the header is not found from the middleware, try and populate it from the request
 	return HxRequestHeaderFromRequest(r)
+}
+
+func (x HxRequestHeaderKey) String() string {
+	return string(x)
 }
